75833bb7e0fe62238eec060c438249e2f713418b
SleepyMoru <87416908+NeMoru05@users.noreply.github.com>
2023-10-08 13:07:31 +0800

    style:pop-up

---
+++ js/main.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginButton = document.getElementById('login-button');
+    const overlay = document.getElementById('overlay-pop');
+    const closeButton = document.getElementById('closeButton');
+
+    // Function to open the login pop-up
+    function openPopup() {
+        overlay.style.display = 'block';
+    }
+
+    // Function to close the login pop-up
+    function closePopup() {
+        overlay.style.display = 'none';
+    }
+
+    // Event listener to open the pop-up when the "Log In" button is clicked
+    loginButton.addEventListener('click', openPopup);
+
+    // Event listener to close the pop-up when the close button is clicked
+    closeButton.addEventListener('click', closePopup);
+
+    // Event listener to close the pop-up when the user clicks outside of it
+    window.addEventListener('click', function(event) {
+        if (event.target === overlay) {
+            closePopup();
+        }
+    });
+});
