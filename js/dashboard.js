4a2fd89df0a4626987510054992357878651529e
L1M1N4L <127649044+L1M1N4L@users.noreply.github.com>
2023-12-02 11:08:23 +0700

    added date

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,23 @@
+//
+// Card on click
+//
 const wrapper = document.querySelector('.wrapper');
 
 wrapper.addEventListener('click', () => {
   wrapper.classList.toggle('clicked');
 });
+
+
+//
+// Get the current date
+//
+   const currentDate = new Date();
+
+   const monthNames = [
+       "January", "February", "March", "April", "May", "June",
+       "July", "August", "September", "October", "November", "December"
+   ];
+
+   const formattedDate = `${monthNames[currentDate.getMonth()]} ${currentDate.getDate()}, ${currentDate.getFullYear()}`;
+
+   document.getElementById("date").textContent = formattedDate;
